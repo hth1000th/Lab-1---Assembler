4a816ba588719c0c575818f9ae60d997f343d937
hth1000th <tahwang@puc.edu>
2016-04-09 23:03:46 -0700

    Test for assembler with specific examples

---
+++ test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include <opcode.h>
+#include <test.h>
+
+int main(void) {
+
+  	test_getOpcode();
+
+  	puts("testing complete");
+  	return 0;
+}
+
+void test_getOpcode() {
+  	puts("testing int getOpcode(char * command)");
+  	assert(getOpcode("LDD") == 1);
+    assert(getOpcode("CAT") == -1);
+  	assert(getOpcode("STO") == 3);
+}
