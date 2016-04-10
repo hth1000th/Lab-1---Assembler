fd5d1fa68ca771eb5919039d50a0f83199eeda79
hth1000th <tahwang@puc.edu>
2016-04-09 23:03:06 -0700

    To find out the opcode for the assembly commands

---
+++ opcode.c
@@ -0,0 +1,18 @@
+#include "opcode.h"
+#include <string.h>
+
+int getOpcode(char * command)
+{
+	char *commands[15] = {"NOP", "LDD", "LDI", "STO", "MOV", "ADI", "ADF", "OR", "AND", "XOR", "ROR", "JMP", "HLT", "LDX", "STX"};
+  char *(*ptr)[15] = &commands;
+  int i;
+  int j = -1;
+
+  for(i=0; i<16; i++) {
+    if(strcasecmp(command, (*ptr)[i]) == 0) {
+      j = i;
+      break;
+    }
+  }
+  return j;
+}
---
+++ opcode.h
@@ -0,0 +1,1 @@
+int getOpcode(char * command);
