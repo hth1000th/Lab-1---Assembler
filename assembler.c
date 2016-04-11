3c850bbe7da9a49a0be5bcffc165e8b24fce6a4b
hth1000th <tahwang@puc.edu>
2016-04-10 22:38:07 -0700

    main file for running assembler(Modified)

--- assembler.c
+++ assembler.c
@@ -1,40 +1,64 @@
 #include "opcode.h"
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(void)
 {
-  FILE *fp1;
-  FILE *fp2;
-  char str[60];
-  char *token;
+		FILE *fp1 = fopen("ADD1.ASM","r+"); // Open the assembly input file;
+  	FILE *fp2 = fopen("ADD1.LST","w+"); // Open the listing output file;
+  	FILE *fp3 = fopen("ADD1.OBJ","w+"); // Open the object output file;
+  	char header[] = "Mem Opcode\tSource"; // the header line;
+  	char str[60];
+  	char *token, *memo;
+		char regit;
+		int i, opcode, op, address;
 
-  fp1 = fopen("ADD1.ASM","r+");
-  fp2 = fopen("ADD2.ASM","w+");
+  	if(fp1 == NULL) {
+  		puts("Error opening file1");
+  		return -1;
+  	}
 
-  if(fp1 == NULL) {
-  	puts("Error opening file1");
-  	return -1;
-  }
+  	puts(header);
+		fputs(header, fp2);
+		fputs("\n", fp2);
+		address = 00;
 
-  while(!feof(fp1)) {
-  	fgets(str, 60, fp1);
-  	puts(str);
-  	fputs(str, fp2);
-  }
-  fclose(fp1);
-  fp1 = fopen("ADD1.ASM","r+");
+		while(!feof(fp1)) {
+      fgets(str, 60, fp1);
+			if(strncmp(str,";",1) == 0 || strncmp(str,"\n",1) == 0) {
+        printf("0%d\t\t",address);
+				fprintf(fp2, "0%d\t\t\t\t\t", address);
+	  		puts(str);
+	  		fputs(str, fp2);
+      }
+      else {
+        token = strtok(str, " ,=+\t\n");
+			  while(token!=NULL){
+          if(strncmp(token,";",1) == 0) {
+            break;
+					}
+					op = getOpcode(token);
 
-  while(!feof(fp1)) {
-    fgets(str, 60, fp1);
-    token = strtok(str, " ,");
-    while(token != NULL) {
-      int opcode = getOpcode(token);
-      printf("%s: %d\n", token, opcode);
-      token = strtok(NULL, " ,");
-    }
-  }
+			    if(strncmp(token,"R",1) == 0){
+            regit = token[1];
+					}
+					else if(op == -1) {
+						memo = token;
+					}
+					else {
+						opcode = op;
+					}
+					token = strtok(NULL, " ,=+\t\n");
+				}
+				printf("0%d %d%c %s\n", i, opcode, regit, memo);
+				str[60] = '0';
+			}
+		}
 
-  fclose(fp2);
-  return 0;
+    fclose(fp1);
+  	fclose(fp2);
+  	fclose(fp3);
+
+  	return 0;
 }
