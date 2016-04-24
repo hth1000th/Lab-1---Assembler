c5d91ca66a3c53fcab7bf2f5c9013250f73a98d7
hth1000th <tahwang@puc.edu>
2016-04-23 19:58:02 -0700

    header file for helper functions

---
+++ helper_functions.h
@@ -0,0 +1,9 @@
+int is_comment(char * str);
+int is_blank(char * str);
+int is_register(char * token);
+void increment_address(int address[]);
+void command_group1(char memo[]);
+void get_two_tokens(char *token[]);
+void command_group3(char memo[]);
+void command_group24(char memo[]);
+void select_commands(int opcode, char memo[]);
